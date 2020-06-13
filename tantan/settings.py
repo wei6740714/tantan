82d97bb35800581e8e52a109ff216e6aa3b425b7
wei67470714 <316015009@qq.com>
2020-06-13 12:43:07 +0800

    log完成

--- tantan/settings.py
+++ tantan/settings.py
@@ -128,3 +128,73 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_URL='media/'
+
+
+#########################
+## Django Logging  BEGIN
+#########################
+
+# LOGGING_DIR 日志文件存放目录
+LOGGING_DIR = os.path.join(BASE_DIR, "logs")
+
+if not os.path.exists(LOGGING_DIR):
+    os.mkdir(LOGGING_DIR)
+
+import logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(levelname)s %(asctime)s %(filename)s %(funcName)s %(lineno)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+    },
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file_handler': {
+             'level': 'INFO',
+             'class': 'logging.handlers.TimedRotatingFileHandler',
+             'filename': '%s/django.log' % LOGGING_DIR,
+             'formatter':'standard',
+             'encoding': 'utf-8'
+        }, # 用于文件输出
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+             'formatter':'standard'
+        },
+    },
+    'loggers': {
+        'mdjango': {
+            'handlers': ['file_handler'],
+            'level':'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            # 一个记录器中可以使用多个处理器
+            'handlers': ['console','mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    }
+}
+
+
+#########################
+## Django Logging  END
+#########################
